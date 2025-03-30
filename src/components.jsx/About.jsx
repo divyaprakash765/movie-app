a360b38d39f9cc59e93748327f8e21a46281386f
divya prakash <2022ugce092@nitjsr.ac.in>
2025-03-30 13:33:07 +0530

    updated

--- src/components.jsx/About.jsx
+++ src/components.jsx/About.jsx
@@ -6,11 +6,11 @@ function About() {
             <div className="bg-zinc-800 rounded-md shadow-md p-10">
                 <h1 className="text-3xl font-semibold text-zinc-200 mb-5">About Us</h1>
                 <p className="text-lg leading-7">
-                    Welcome to <span className="text-purple-400 font-bold">Web App</span>, your ultimate hub for discovering trending movies, TV shows, and popular content. 
+                    Welcome to <span className="text-purple-400 font-bold">Movie App</span>, your ultimate hub for discovering trending movies, TV shows, and popular content. 
                     Our platform is designed to make it easy for users to find detailed information, trailers, and cast profiles.
                 </p>
                 <p className="text-lg leading-7 mt-4">
-                    Whether you’re a movie enthusiast or a TV series binge-watcher, <span className="text-purple-400 font-bold">Web App</span> brings all the latest updates to your screen. 
+                    Whether you’re a movie enthusiast or a TV series binge-watcher, <span className="text-purple-400 font-bold">Movie App</span> brings all the latest updates to your screen. 
                     Explore new content, learn about your favorite stars, and dive into the world of entertainment.
                 </p>
             </div>
